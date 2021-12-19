78f9ed5e0593d8d9610ab4c3384682be25d28f4c
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2021-12-19 14:49:40 -0300

    subindo

--- Documetation_of_sqlite3/coneccao.py
+++ Documetation_of_sqlite3/coneccao.py
@@ -1,44 +1,43 @@
 import sqlite3
+from sqlite3 import Error
 import os
+os.system("clear")
 
-# Quando é baixado o anaconda, ele instala o sqlite3
+# Objetivo criar conexão e criar tabela
 
-PATH = "/home/lucas/Documentos/Github/BancoDeDados/escola.db"
+## Caminho do banco de dados
+pathBanco = "/home/lucas/Documentos/Github/BancoDeDados/Documetation_of_sqlite3/banco.db"
 
-# Essa funçao verifica se existe um banco 
-# de dados no caminho especificado
-# na variável PATH. 
-# Caso esse banco exista, ele será apagado.
-def existeBanco(PATH):
-    os.remove(PATH) if os.path.exists(PATH) else None
+## Criar uma conexão
 
-
-# Criando uma conexão chamada con
-# Passando o caminho aonde o banco de dados vai ser criado
-# esse caminho pode ser colocado em qualquer lugar
-def coneccao(PATH):
-    con = sqlite3.connect(PATH)
+def conexaoBanco():
+    try:
+        con = sqlite3.connect(pathBanco)
+    except Error as err:
+        print(err)
     return con
 
-# 
-def fecharConeccao(con):
-    con.close()
+con = conexaoBanco()
 
+## Comando para criar tabela
 
-# Criando um cursor para percorrer todos os registro do banco de dados
-def cursorBanco(con):
-    return con.cursor()
+create_table = """create table teste(
+    id integer primary key autoincrement,
+    contato varchar(30),
+    telefone varchar(14),
+    email varchar(30)
+);
+"""
 
-# criando uma instancia da classe conecção do sqlite3 
-con1 = coneccao(PATH)
+def criarTable(sql):
+    try:
+        cur = con.cursor()
+        cur.execute(sql)
+    except Error as err:
+        print(err)
+    else:
+        print("Tabela Criada")
 
-# criando uma instancia da classe cursor, para 
-# fazer as consultas no banco de dados
-cur = cursorBanco(con1)
-
-sql_create = f'create table cursos '\
-        '(id integer primary dey, '\
-        'titulo varchar(100) '\
-        'categoria varchar(140))'
-
-cur.execute(sql_create)
+criarTable(create_table)
+con.commit()
+con.close()
